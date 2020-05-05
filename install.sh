d525e53a47bfab5ce3844133cda77af65810845e
Andrew W. Burger <andrew.burger@awbtech.org>
2020-05-05 15:37:39 -0700

    Repair Install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 chmod +x run.sh
-export PATH="$PATH:/root/reverseproxy
+export PATH="$PATH:/root/reverseproxy"
 sudo ln -s /root/reverseproxy/manage.py manage
 cp reverseproxy.service /etc/systemd/system/reverseproxy.service
 systemctl enable reverseproxy.service
